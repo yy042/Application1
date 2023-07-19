72a09994ce3f6ee5c16b7f7d0e10b45b298255d4
yy042 <1030322547@qq.com>
2023-07-19 16:13:41 +0800

    set up MainActivity.kt-services recyclerview

--- app/src/main/java/cn/edu/fzu/application1/MainActivity.kt
+++ app/src/main/java/cn/edu/fzu/application1/MainActivity.kt
@@ -1,14 +1,21 @@
 package cn.edu.fzu.application1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val str:String="Bobb"
-        println(str)
+        //设置服务recyclerview
+        var rv_services:RecyclerView=findViewById(R.id.rv_services)
+        var servicesLayoutManager:LinearLayoutManager=
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rv_services.layoutManager = servicesLayoutManager
+
+
     }
 }
