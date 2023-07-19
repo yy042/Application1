27d5871f8a7133476e42f7e305f0debc4ff03437
yy042 <1030322547@qq.com>
2023-07-19 16:40:21 +0800

    use ViewBinding to set up MainActivity.kt and rvServices.

--- app/src/main/java/cn/edu/fzu/application1/MainActivity.kt
+++ app/src/main/java/cn/edu/fzu/application1/MainActivity.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import cn.edu.fzu.application1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //设置服务recyclerview
-        var rv_services:RecyclerView=findViewById(R.id.rv_services)
+        //设置rvServices
         var servicesLayoutManager:LinearLayoutManager=
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        rv_services.layoutManager = servicesLayoutManager
-
-
+        binding.rvServices.layoutManager = servicesLayoutManager //用 binding.rvServices 替换 rv_service
     }
 }
