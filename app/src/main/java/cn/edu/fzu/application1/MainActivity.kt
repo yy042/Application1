30e067b322adbcdd4d7b48b066b78e8e078ddb6d
yy042 <1030322547@qq.com>
2023-07-20 10:59:34 +0800

    rvServices has been set up successfully.

--- app/src/main/java/cn/edu/fzu/application1/MainActivity.kt
+++ app/src/main/java/cn/edu/fzu/application1/MainActivity.kt
@@ -3,20 +3,48 @@ package cn.edu.fzu.application1
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import cn.edu.fzu.application1.adapter.RvServicesAdapter
 import cn.edu.fzu.application1.databinding.ActivityMainBinding
+import cn.edu.fzu.application1.entity.ItemService
+import cn.edu.fzu.application1.util.Util
+import cn.edu.fzu.application1.util.Util.setupRecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var rvServiceAdapter: RvServicesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //设置rvServices
+        //设置recyclerview
         var servicesLayoutManager:LinearLayoutManager=
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         binding.rvServices.layoutManager = servicesLayoutManager //用 binding.rvServices 替换 rv_service
+        //初始化adapter
+        rvServiceAdapter= RvServicesAdapter(R.layout.item_service,mutableListOf())
+        binding.rvServices.adapter=rvServiceAdapter
+
+        //给adapter添加数据
+        val data = listOf(
+            ItemService("充流量", R.drawable.ic_mobiledata, "流量告急速订购"),
+            ItemService("开通自动充", R.drawable.ic_autorecharge, "专治忘充值"),
+            ItemService("电子发票", R.drawable.ic_receipt, "批量开票不排队"),
+            ItemService("充值记录", R.drawable.ic_bill, "可查全网记录")
+        )
+        rvServiceAdapter.setList(data)
+
+        setupRecyclerView(
+            binding.rvServices, //传入recyclerView对象
+            rvServiceAdapter,
+            listOf( //传入数据列表
+                ItemService("充流量", R.drawable.ic_mobiledata, "流量告急速订购"),
+                ItemService("开通自动充", R.drawable.ic_autorecharge, "专治忘充值"),
+                ItemService("电子发票", R.drawable.ic_receipt, "批量开票不排队"),
+                ItemService("充值记录", R.drawable.ic_bill, "可查全网记录")
+            )
+        )
     }
 }
