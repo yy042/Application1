3171e6fcf2e10f65a2f3b3fc0d1dedf11b59a9cf
yy042 <1030322547@qq.com>
2023-07-20 11:44:32 +0800

    rvTasks without icon background was done.

--- app/src/main/java/cn/edu/fzu/application1/MainActivity.kt
+++ app/src/main/java/cn/edu/fzu/application1/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import cn.edu.fzu.application1.adapter.RvServicesAdapter
+import cn.edu.fzu.application1.adapter.RvTasksAdapter
 import cn.edu.fzu.application1.databinding.ActivityMainBinding
 import cn.edu.fzu.application1.entity.ItemService
+import cn.edu.fzu.application1.entity.ItemTask
 import cn.edu.fzu.application1.util.Util
 import cn.edu.fzu.application1.util.Util.setupRecyclerView
 
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var rvServiceAdapter: RvServicesAdapter
+    private lateinit var rvTaskAdapter: RvTasksAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +32,20 @@ class MainActivity : AppCompatActivity() {
                 ItemService("开通自动充", R.drawable.ic_autorecharge, "专治忘充值"),
                 ItemService("电子发票", R.drawable.ic_receipt, "批量开票不排队"),
                 ItemService("充值记录", R.drawable.ic_bill, "可查全网记录")
-            )
+            ),
+            LinearLayoutManager.HORIZONTAL
+        )
+
+        //设置RvTasks
+        rvTaskAdapter= RvTasksAdapter(R.layout.item_task,mutableListOf())
+        setupRecyclerView(
+            binding.rvTasks, //传入recyclerView对象
+            rvTaskAdapter,
+            listOf( //传入数据列表
+                ItemTask("开启自动充值得金豆", R.drawable.ic_activaterecharge, "首次开启得","0","10金豆","去开启"),
+                ItemTask("看科普视频得金豆", R.drawable.ic_video, "15s得多看多得...","0/6","5金豆","去看看")
+            ),
+            LinearLayoutManager.VERTICAL
         )
     }
 }
