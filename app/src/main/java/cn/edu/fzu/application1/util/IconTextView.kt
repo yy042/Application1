786d96e79b94e2974e5ad7d4a21a4fb6c369398c
yy042 <1030322547@qq.com>
2023-07-26 09:56:06 +0800

    the check balance button's animation and coroutine are done.

--- app/src/main/java/cn/edu/fzu/application1/util/IconTextView.kt
+++ app/src/main/java/cn/edu/fzu/application1/util/IconTextView.kt
@@ -5,20 +5,17 @@ import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.animation.Animation
 import android.view.animation.LinearInterpolator
 import android.view.animation.RotateAnimation
-import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
 import cn.edu.fzu.application1.R
 import cn.edu.fzu.application1.databinding.IconTextviewBinding
 import kotlinx.coroutines.*
 
 //自定义View类
 class IconTextView(context: Context, attrs: AttributeSet) :
-    LinearLayout(context, attrs) , CoroutineScope by MainScope(){
+    LinearLayout(context, attrs), CoroutineScope by MainScope() {
 
     //初始化绑定类
     private val binding = IconTextviewBinding.inflate(LayoutInflater.from(context), this, true)
@@ -29,10 +26,19 @@ class IconTextView(context: Context, attrs: AttributeSet) :
 
         //获取自定义属性的值
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.IconTextView)
-        val icLoadingSrc = typedArray.getResourceId(R.styleable.IconTextView_icLoadingSrc, R.id.ic_loading) // 获取图片资源的值，如果没有指定，默认为0
-        val icLoadingSize = typedArray.getDimensionPixelSize(R.styleable.IconTextView_icLoadingSize, dpToPx(16)) // 获取图片尺寸的值，如果没有指定，默认为16dp
+        val icLoadingSrc = typedArray.getResourceId(
+            R.styleable.IconTextView_icLoadingSrc,
+            R.id.ic_loading
+        ) // 获取图片资源的值，如果没有指定，默认为0
+        val icLoadingSize = typedArray.getDimensionPixelSize(
+            R.styleable.IconTextView_icLoadingSize,
+            dpToPx(16)
+        ) // 获取图片尺寸的值，如果没有指定，默认为16dp
         val text = typedArray.getString(R.styleable.IconTextView_text) // 获取文字内容的值，如果没有指定，默认为null
-        val textColor = typedArray.getColor(R.styleable.IconTextView_textColor, Color.parseColor("#fefefe")) // 获取文字颜色的值，如果没有指定，默认为黑色
+        val textColor = typedArray.getColor(
+            R.styleable.IconTextView_textColor,
+            Color.parseColor("#fefefe")
+        ) // 获取文字颜色的值，如果没有指定，默认为黑色
         typedArray.recycle() // 回收TypedArray对象
 
         //根据自定义属性的值来设置ImageView和TextView
@@ -44,14 +50,16 @@ class IconTextView(context: Context, attrs: AttributeSet) :
 
         }
         if (text != null) {
-            binding.tvCheck.text = text // 如果文字内容不为null，设置文字内容
+            setText(text) // 如果文字内容不为null，设置文字内容
         }
         binding.tvCheck.setTextColor(textColor) // 设置文字颜色
 
         //创建旋转动画
-        val rotateAnimation = RotateAnimation(0f, 360f,
+        val rotateAnimation = RotateAnimation(
+            0f, 360f,
             Animation.RELATIVE_TO_SELF, 0.5f,
-            Animation.RELATIVE_TO_SELF, 0.5f)
+            Animation.RELATIVE_TO_SELF, 0.5f
+        )
         rotateAnimation.duration = 1000
         rotateAnimation.repeatCount = Animation.INFINITE
         rotateAnimation.interpolator = LinearInterpolator()
@@ -61,7 +69,7 @@ class IconTextView(context: Context, attrs: AttributeSet) :
             //启动或停止旋转动画
             if (binding.icLoading.animation == null) {
                 binding.icLoading.visibility = View.VISIBLE //显示loading图片
-                binding.tvCheck.text = "查询余额" //更新文字内容
+                setText("查询余额" ) //更新文字内容
                 binding.icReload.visibility = View.GONE //隐藏重新加载图片
                 binding.icLoading.startAnimation(rotateAnimation)
                 //启动一个协程来模拟网络请求
@@ -69,18 +77,21 @@ class IconTextView(context: Context, attrs: AttributeSet) :
                     delay(3000L) //延迟3秒
                     binding.icLoading.clearAnimation() //停止旋转动画
                     binding.icLoading.visibility = View.GONE //隐藏loading图片
-                    binding.tvCheck.text = "余额：58.00元" //更新文字内容
+                    setText("余额：58.00元")  //更新文字内容
                     binding.icReload.visibility = View.VISIBLE //显示重新加载图片
                 }
             } else {
                 binding.icLoading.clearAnimation()
-                binding.tvCheck.text = "查询余额" //更新文字内容
+                setText("查询余额")  //更新文字内容
                 binding.icLoading.visibility = View.VISIBLE //显示loading图片
                 binding.icReload.visibility = View.GONE //隐藏重新加载图片
             }
         }
     }
 
+    private fun setText(text:String){
+        binding.tvCheck.text =text
+    }
     //将dp转换为px的辅助方法
     private fun dpToPx(dp: Int): Int {
         return (dp * resources.displayMetrics.density).toInt()
